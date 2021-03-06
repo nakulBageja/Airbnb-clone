e2115ad1c6dc8d3f22f777428dfccc3687ee8628
Nakul Bageja <nbageja@gmail.com>
2021-03-06 14:09:51 +0530

    Addition of google place api

--- src/screens/DestinationLocations/index.js
+++ src/screens/DestinationLocations/index.js
@@ -4,38 +4,59 @@ import styles from './styles';
 import destinationData from '../../../assets/data/search';
 import Entypo from 'react-native-vector-icons/Entypo';
 import {useNavigation} from '@react-navigation/native';
+import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
 const DestinationLocationsScreen = () => {
   const [destinationInput, setDestinationInput] = useState('');
   const navigation = useNavigation();
   const renderList = (item) => {
     return (
+      //     {/* List of locations */}
+      // <Pressable
+      //   onPress={() => {
+      //     navigation.navigate('Guests');
+      //   }}>
       <View style={styles.listContainer}>
         <View style={styles.iconContainer}>
           <Entypo name="location-pin" size={25} />
         </View>
         <Text style={styles.locationText}>{item.description}</Text>
       </View>
+      //    </Pressable>
     );
   };
   return (
-    <Pressable
-      onPress={() => {
-        navigation.navigate('Guests');
-      }}
-      style={styles.container}>
-      {/* Input field */}
-      <TextInput
-        style={styles.textInput}
+    <View style={styles.container}>
+      <GooglePlacesAutocomplete
         placeholder="Where are you going?"
-        value={destinationInput}
-        onChangeText={setDestinationInput}
+        onPress={(data, details = null) => {
+          // 'details' is provided when fetchDetails = true
+          navigation.navigate('Guests');
+          console.log(data, details);
+        }}
+        fetchDetails //To get more information of that place
+        styles={{
+          textInput: styles.textInput,
+        }}
+        query={{
+          key: 'AIzaSyD3cDc6BPrUXhXUhVF17lTvjvbHuRneqw8',
+          language: 'en',
+          type: '(cities)',
+        }}
+        suppressDefaultStyles //This is added to remove default styles
+        renderRow={(item) => renderList(item)}
       />
-      {/* List of locations */}
-      <FlatList
+      {/* Input field
+        {/* <TextInput
+          style={styles.textInput}
+          placeholder="Where are you going?"
+          value={destinationInput}
+          onChangeText={setDestinationInput}
+        /> */}
+      {/* <FlatList
         data={destinationData}
         renderItem={({item}) => renderList(item)}
-      />
-    </Pressable>
+      /> */}
+    </View>
   );
 };
 
