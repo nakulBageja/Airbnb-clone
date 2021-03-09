fcb421f4b7c7dc9389d18530ad7c1d42fe0d094f
Nakul Bageja <nbageja@gmail.com>
2021-03-09 11:58:29 +0530

    Addition of map focus on selected region

--- src/components/PostsCarousel/Posts/styles.js
+++ src/components/PostsCarousel/Posts/styles.js
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
     elevation: 10,
   },
   container: {
-    height: '100%',
+    height: '40%',
     padding: 5,
   },
   bedrooms: {
--- src/screens/SearchResultsMap/index.js
+++ src/screens/SearchResultsMap/index.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useRef, useState} from 'react';
 import {View, Text, FlatList, useWindowDimensions} from 'react-native';
-import MapView, {Marker} from 'react-native-maps';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 import places from '../../../assets/data/feed';
 import CustomMarker from '../../components/CustomMarker';
 import PostsCarousel from '../../components/PostsCarousel/Posts';
@@ -8,31 +8,63 @@ const SearchResultsMap = () => {
   const [isSelectedId, setIsSelectedId] = useState(null);
   const width = useWindowDimensions().width;
 
+  // const flatList = useRef();
+  // const viewConfig = useRef({
+  //   itemVisiblePercentThreshold: 90,
+  //   // waitForInteraction: true,
+  // }); //taking those item value only who is available 70%
+  // const onViewChanged = useRef(({viewableItems}) => {
+  //   console.log(viewableItems);
+  //   // Whenever the view changes select that item in the carousel
+  //   if (viewableItems.length > 0) {
+  //     const selectedPlace = viewableItems[0].item;
+  //     setIsSelectedId(selectedPlace.id);
+  //   }
+  // });
+
+  // useEffect(() => {
+  //   if (!isSelectedId || !flatList) return;
+  //   const index = places.findIndex((place) => place.id === isSelectedId); //find the selected id
+  //   flatList.current.scrollToIndex({index}); //move the carousel to that item
+  // }, [isSelectedId]);
+
   // to use values of flatlist
-  const flatList = useRef();
-  const viewConfig = useRef({
-    itemVisiblePercentThreshold: 90,
-    // waitForInteraction: true,
-  }); //taking those item value only who is available 70%
-  const onViewChanged = useRef((viewableItems) => {
-    console.log(viewableItems);
-    // Whenever the view changes select that item in the carousel
-    // if (viewableItems.length > 0) {
-    //   const selectedPlace = viewableItems[0].item;
-    //   setIsSelectedId(selectedPlace.id);
-    // }
-  });
+  const flatlist = useRef();
+
+  const map = useRef(); //change map focus to the place selected
+
+  // const viewConfig = useRef({itemVisiblePercentThreshold: 70});
+  // const onViewChanged = useRef(({viewableItems}) => {
+  //   if (viewableItems.length > 0) {
+  //     const selectedPlace = viewableItems[0].item;
+  //     setIsSelectedId(selectedPlace.id);
+  //   }
+  // });
 
   useEffect(() => {
-    if (!isSelectedId || !flatList) return;
-    const index = places.findIndex((place) => place.id === isSelectedId); //find the selected id
-    flatList.current.scrollToIndex({index}); //move the carousel to that item
+    if (!isSelectedId || !flatlist) {
+      return;
+    }
+    const index = places.findIndex((place) => place.id === isSelectedId);
+    flatlist.current.scrollToIndex({index});
+
+    const selectedPlace = places[index];
+    //focusing on the region selected
+    const region = {
+      latitude: selectedPlace.latitude,
+      longitude: selectedPlace.longitude,
+      latitudeDelta: 0.8,
+      longitudeDelta: 0.8,
+    };
+    map.current.animateToRegion(region);
   }, [isSelectedId]);
 
   return (
     <View style={{width: '100%', height: '100%'}}>
       <MapView
+        ref={map}
         style={{width: '100%', height: '100%'}}
+        provider={PROVIDER_GOOGLE}
         initialRegion={{
           latitude: 28.3915637,
           longitude: -16.6291304,
@@ -51,7 +83,7 @@ const SearchResultsMap = () => {
       </MapView>
       <View style={{position: 'absolute', bottom: 10}}>
         <FlatList
-          ref={flatList}
+          ref={flatlist}
           data={places}
           renderItem={({item}) => <PostsCarousel post={item} key={item.id} />}
           horizontal
@@ -59,8 +91,8 @@ const SearchResultsMap = () => {
           snapToInterval={width - 60}
           snapToAlignment={'center'}
           decelerationRate={'fast'}
-          viewabilityConfig={viewConfig.current}
-          onViewableItemsChanged={onViewChanged.current}
+          // viewabilityConfig={viewConfig.current}
+          // onViewableItemsChanged={onViewChanged.current}
         />
       </View>
     </View>
